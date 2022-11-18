00a0ecf02c9860786094585d0c98e4179d3f7e89
antarcticrobot <antarcticrobot@163.com>
2022-11-18 16:01:55 +0800

    微调二维码读写的代码

--- scripts/qr_code/read_qr.py
+++ scripts/qr_code/read_qr.py
@@ -1,29 +1,36 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-'''
-读取二维码
-'''
 import cv2
 
-# 读取二维码图片
-img = cv2.imread("pic1.jpg")
-# 初始化cv2的二维码检测器
-detector = cv2.QRCodeDetector()
-# 解码
-data, bbox, straight_qrcode = detector.detectAndDecode(img)
-# 如果解码成功
-if bbox is not None:
-    print(f"QRCode data:\n{data}")
-    # 用线条显示图像
-    # 边框长度
-    n_lines = len(bbox)
-    for i in range(n_lines):
-        # 输出
-        point1 = tuple(bbox[i][0])
-        point2 = tuple(bbox[(i + 1) % n_lines][0])
-        # cv2.line(img, point1, point2, color=(255, 0, 0), thickness=2)
-# 显示结果
-cv2.imshow("img", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def read_qr_code(img):
+    detector = cv2.QRCodeDetector()
+    data, bbox, straight_qrcode = detector.detectAndDecode(img)
+    if bbox is not None:
+        print(f"QRCode data:\n{data}")
+        print(f"bbox:\n{bbox}")
+        print(f"straight_qrcode.shape:\n{straight_qrcode.shape}")
+        n_codes = len(bbox)
+        for num in range(n_codes):
+            n_lines = len(bbox[num])
+            for i in range(n_lines):
+                point1 = bbox[0][i]
+                point1 = int(point1[0]), int(point1[1])
+                point2 = bbox[0][(i + 1) % n_lines]
+                point2 = int(point2[0]), int(point2[1])
+                cv2.line(img, point1, point2, color=(0, 255, 0), thickness=2)
+
+        cv2.imwrite("result.jpg", img)
+        cv2.imwrite("straight_qrcode.jpg", straight_qrcode)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+        cv2.imshow("result", img)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    img = cv2.imread("test4.png")
+    # cv2.namedWindow("result", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+    # cv2.imshow("result", img)
+    # cv2.waitKey(0)
+    read_qr_code(img)
