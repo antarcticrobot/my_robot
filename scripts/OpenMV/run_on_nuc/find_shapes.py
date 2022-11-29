e899df801a7565097c63873d1c5ce103ff80e2b4
antarcticrobot <antarcticrobot@163.com>
2022-11-29 17:40:58 +0800

    为find_shapes.py增加局部温度均值输出。

--- scripts/OpenMV/run_on_nuc/find_shapes.py
+++ scripts/OpenMV/run_on_nuc/find_shapes.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+from helper import *
 
 
-def ShapeDetection(img,imgContour):
+def ShapeDetection(img, imgContour):
     contours, hierarchy = cv2.findContours(
         img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for obj in contours:
@@ -11,7 +12,6 @@ def ShapeDetection(img,imgContour):
         approx = cv2.approxPolyDP(obj, 0.02*perimeter, True)
         CornerNum = len(approx)
         x, y, w, h = cv2.boundingRect(approx)
-
         if CornerNum == 3:
             objType = "triangle"
         elif CornerNum == 4:
@@ -24,10 +24,28 @@ def ShapeDetection(img,imgContour):
         else:
             objType = "N"
 
+        g = np.mean(imgContour[x:x+w, y:y+h])
+        print('avg of roi', g, map_g_to_temp(g))
+
+        g = np.mean(imgContour[0:x])
+        print('avg of roi', g, map_g_to_temp(g))
+        g = np.mean(imgContour[x:x+w])
+        print('avg of roi', g, map_g_to_temp(g))
+        g = np.mean(imgContour[x+w:120])
+        print('avg of roi', g, map_g_to_temp(g))
+        g = np.mean(imgContour[:][0:y])
+        print('avg of roi', g, map_g_to_temp(g))
+        g = np.mean(imgContour[:][y:y+h])
+        print('avg of roi', g, map_g_to_temp(g))
+        g = np.mean(imgContour[:][y+h:160])
+        print('avg of roi', g, map_g_to_temp(g))
+
         cv2.rectangle(imgContour, (x, y), (x+w, y+h), (0, 0, 255), 2)
         cv2.putText(imgContour, objType, (x+(w//2), y+(h//2)),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 1)
-        print(CornerNum,)
+                    cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 0), 1)
+        print(x, y, w, h)
+        print(CornerNum, objType, area)
+
 
 def process_img(fileName):
     img = cv2.imread(fileName)
@@ -36,9 +54,9 @@ def process_img(fileName):
     imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, imgBinary = cv2.threshold(
         imgGray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
-    imgBlur = cv2.GaussianBlur(imgBinary, (5, 5), 1) 
-    imgCanny = cv2.Canny(imgBlur, 60, 60) 
-    ShapeDetection(imgCanny,imgContour)
+    imgBlur = cv2.GaussianBlur(imgBinary, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 60, 60)
+    ShapeDetection(imgCanny, imgContour)
 
     cv2.imshow("Original img", img)
     cv2.imshow("imgGray", imgGray)
@@ -48,8 +66,7 @@ def process_img(fileName):
 
     cv2.waitKey(0)
 
+
 path = '/home/yr/热成像数据_存档/2022_11_28_1100_tqyb17/raw'
 fileName = path+'/1151506.pgm'
 process_img(fileName)
-
-
