e53e1a5cb70c266c6562de745c082839eab0bf16
antarcticrobot <antarcticrobot@163.com>
2023-03-07 21:30:27 +0800

    主要是调整命名，没改完，先存一下

--- scripts/OpenMV/Compression/compression_png_fuc.py
+++ scripts/OpenMV/Compression/compression_png_fuc.py
@@ -3,44 +3,55 @@ import os
 import numpy as np
 
 
-# 对图像作反色，几乎完全无效
-def image_reverse(path_name, img):
-    cur_name = path_name+"_reverse_"+str(cnt)+".png"
-    cv2.imwrite(cur_name, 255-img, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
-    size = os.path.getsize(cur_name)
-    print("png_reverse: ", cnt, " ", size)
-    return cur_name
+# # 对图像作反色，几乎完全无效
+# def image_reverse(num, save_path, img):
+#     cur_name = save_path+str(num)+"_reverse_"+str(cnt)+".png"
+#     cv2.imwrite(cur_name, 255-img, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+#     size = os.path.getsize(cur_name)
+#     print("png_reverse: ", cnt, " ", size)
+#     return cur_name
 
 
-# 对图像作除法，效果明显
-def image_div(path_name, img, divisor):
-    cur_name = path_name+"_div"+str(divisor)+'_'+str(cnt)+".png"
-    cv2.imwrite(cur_name, img/divisor, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
-    size = os.path.getsize(cur_name)
-    print("png_div: ", cnt, " ", size)
-    return cur_name
+# # 对图像作除法，效果明显
+# def image_div(num, save_path, img, divisor):
+#     cur_name = save_path+str(num)+"_div"+str(divisor)+'_'+str(cnt)+".png"
+#     cv2.imwrite(cur_name, img/divisor, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+#     size = os.path.getsize(cur_name)
+#     print("png_div: ", cnt, " ", size)
+#     return cur_name
 
 
-# 对图像降低分辨率，效果明显
-def image_reduce_resolution(path_name, img, divisor):
-    cur_name = path_name+"_reduce"+str(divisor)+'_'+str(cnt)+".png"
-
+# 对图像降低分辨率，直接取左上角，效果明显
+def image_reduce_resolution(num, save_path, img, divisor):
+    cur_name = save_path+str(num)+"_reduce"+str(divisor)+'_'+str(cnt)+".png"
     row = int(120/divisor)
     col = int(160/divisor)
     tmp = np.zeros((row, col))
     for i in range(row):
         for j in range(col):
             tmp[i, j] = (img[i*divisor, j*divisor]).astype(np.uint8)
-
     cv2.imwrite(cur_name, tmp, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
     size = os.path.getsize(cur_name)
     print("png_reduce: ", cnt, " ", size)
     return cur_name
 
 
-def test_para_for_png(img, path_name, cnt):
-    cur_name = path_name+"_"+str(cnt)+".png"
-    restore_name = path_name+"_"+str(cnt)+"_restore.bmp"
+# # 对图像降低分辨率，cv2.pyrDown，太影响破损检测
+# def image_pyrDown(num, save_path, img, divisor):
+#     cur_name = "{0}/{1}_{2}_reduce_{3}.png".format(
+#         save_path, num, cnt, divisor)
+#     for i in range(divisor-1):
+#         img = cv2.pyrDown(img)
+#     cv2.imwrite(cur_name, img, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+#     size = os.path.getsize(cur_name)
+#     print("png_pyrDown: ", cnt, " ", size)
+#     return cur_name
+
+
+def test_para_for_png(num, save_path, img, cnt):
+    prefix_name = "{0}/{1}_{2}".format(save_path, num, cnt)
+    cur_name = "{0}.png".format(prefix_name)
+    restore_name = "{0}_restore.bmp".format(prefix_name)
 
     cv2.imwrite(cur_name, img, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
     size = os.path.getsize(cur_name)
@@ -48,29 +59,36 @@ def test_para_for_png(img, path_name, cnt):
 
     # cv2.imwrite(restore_name, cv2.imread(cur_name, 0))
 
-    # cur_name = image_reverse(path_name, img)
+    # cur_name = image_reverse(num, save_path, img)
     # cv2.imwrite(restore_name, 255-cv2.imread(cur_name, 0))
 
-    # divisor = 16
-    # cur_name = image_div(path_name, img, divisor)
+    # divisor = 32
+    # cur_name = image_div(num, save_path, img, divisor)
     # cv2.imwrite(restore_name, cv2.imread(cur_name, 0)*divisor)
 
-    divisor = 1
-    cur_name = image_reduce_resolution(path_name, img, divisor)
+    divisor = 2
+    cur_name = image_reduce_resolution(num, save_path, img, divisor)
     cv2.imwrite(restore_name, cv2.imread(cur_name, 0))
+
+    # divisor = 2
+    # cur_name = image_pyrDown(num, save_path, img, divisor)
+    # cv2.imwrite(restore_name, cv2.imread(cur_name, 0))
 
     size = os.path.getsize(restore_name)
     print("restore.bmp: ", size)
 
 
 if __name__ == '__main__':
-    path_name = '/home/yr/热成像数据_存档_排烟管/外裹纸/2023_02_20_1630_pyg/raw/421802'
-    save_name = './selected_pic_for_test_compression_png/output/421802'
-    cur_name = path_name+".bmp"
-    img = cv2.imread(cur_name, 0)
+    read_path = '/home/yr/热成像数据_存档_排烟管/外裹纸/2023_02_20_1630_pyg/raw/'
+    save_path = './selected_pic_for_test_compression_png/output'
 
-    size = os.path.getsize(cur_name)
-    print("bmp: ", size)
+    num_list = [421802]
+    for num in num_list:
+        cur_name = read_path+str(num)+".bmp"
+        img = cv2.imread(cur_name, 0)
 
-    for cnt in range(0, 10):
-        test_para_for_png(img, save_name, cnt)
+        size = os.path.getsize(cur_name)
+        print("bmp: ", size)
+
+        for cnt in range(9, 10):
+            test_para_for_png(num, save_path, img, cnt)
