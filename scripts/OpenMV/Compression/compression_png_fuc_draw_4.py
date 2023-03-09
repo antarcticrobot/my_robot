9b19db49f91af9735a883cd359f1e56dca9c91b8
antarcticrobot <antarcticrobot@163.com>
2023-03-09 21:17:24 +0800

    拆出来了helpers.py 重构了compression_png_fuc_draw_3.py、compression_png_fuc_draw_4.py

--- scripts/OpenMV/Compression/compression_png_fuc_draw_4.py
+++ scripts/OpenMV/Compression/compression_png_fuc_draw_4.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -6,43 +5,7 @@
 import cv2
 import os
 import math
-import numpy as np
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-
-def print_size(str, file_path):
-    size = os.path.getsize(file_path)
-    # print(str, size)
-    return size
-
-
-def do_restrore(result_name, restore_name, multi_para=1):
-    jump_restrore = False
-    if (jump_restrore):
-        return
-    cv2.imwrite(restore_name, cv2.imread(result_name, 0)*multi_para)
-    print_size("restore: ", restore_name)
-
-
-def get_two_names(prefix, fuc, divisor):
-    result_name = "{0}_{1}_{2}.png".format(prefix, fuc, divisor)
-    restore_name = "{0}_{1}_{2}_restore.bmp".format(prefix, fuc, divisor)
-    return result_name, restore_name
-
-
-# 对图像降低分辨率，cv2.pyrDown，太影响破损检测
-def image_pyrDown(img,  prefix, divisor):
-    result_name, restore_name = get_two_names(prefix, "pyrDown", divisor)
-
-    tmp = img
-    for i in range(divisor-1):
-        tmp = cv2.pyrDown(tmp)
-
-    cv2.imwrite(result_name, tmp, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
-    print_size("pyrDown result: ", result_name)
-    do_restrore(result_name, restore_name)
+from helpers import *
 
 
 record_raw = []
@@ -51,75 +14,19 @@ record_reduce = []
 start_png_para = 9
 
 
-# # 对图像作反色，几乎完全无效
-# # 对图像作除法，效果明显
-def image_div(img,  prefix, divisor):
-    result_name, restore_name = get_two_names(prefix, "div", divisor)
-    cv2.imwrite(result_name, img/divisor, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
-    record_div.append(print_size("div result: ", result_name))
-    do_restrore(result_name, restore_name, divisor)
-
-
-# 对图像降低分辨率，直接取左上角，效果明显
-def image_reduce_resolution(img,  prefix, divisor):
-    result_name, restore_name = get_two_names(prefix, "reduce", divisor)
-
-    row = int(120/divisor)
-    col = int(160/divisor)
-    tmp = np.zeros((row, col))
-    for i in range(row):
-        for j in range(col):
-            tmp[i, j] = (img[i*divisor, j*divisor]).astype(np.uint8)
-
-    cv2.imwrite(result_name, tmp, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
-    record_reduce.append(print_size("reduce result: ", result_name))
-    do_restrore(result_name, restore_name)
-
-
-def test_para_for_png(img, num, save_path, cnt):
+def test_para_for_png(img, num, save_path, cnt, record):
     prefix = "{0}/{1}_{2}".format(save_path, num, cnt)
     png_name = "{0}.png".format(prefix)
 
     cv2.imwrite(png_name, img, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
     size = os.path.getsize(png_name)
-    record_raw.append(size)
+    record.append(size)
     print("png: ", cnt, " ", size)
 
     for divisor in range(1, 5):
-        image_div(img, prefix, int(math.pow(2, divisor-1)))
-        image_pyrDown(img,  prefix, divisor)
-        image_reduce_resolution(img,  prefix, divisor)
-
-
-def print_value(rects, offset):
-    for rect in rects:
-        height = rect.get_height()
-        plt.text(rect.get_x()+rect.get_width()/3. +
-                 offset, 1.01*height, '%s' % (height))
-
-
-def drawHistogram():
-    plt.rcParams["font.sans-serif"] = ['SimHei']  # 设置字体
-    plt.rcParams["axes.unicode_minus"] = False  # 正常显示负号
-
-    total_width, n = 0.5, 3   # 柱状图总宽度，有几组数据
-    width = total_width / n   # 单个柱状图的宽度
-    my_fontsize = 16
-    x = np.arange(len(record_div))   # 横坐标范围
-
-    plt.figure()
-    plt.title("")
-    plt.xlabel("png压缩级别", fontsize=my_fontsize)
-    plt.xticks(x, range(start_png_para, 10))  # 设置x轴刻度显示值
-    plt.ylabel("结果图片大小/Byte", fontsize=my_fontsize)
-    rect0 = plt.bar(x - width, record_raw, width=width, label="raw")
-    rect1 = plt.bar(x, record_div, width=width, label="div")
-    rect2 = plt.bar(x + width, record_reduce, width=width, label="reduce")
-    print_value(rect0, -0.05)
-    print_value(rect1, -0.03)
-    print_value(rect2, -0.05)
-    plt.legend()
-    plt.show()
+        image_div(img, prefix, cnt, int(math.pow(2, divisor-1)), record_div)
+        image_pyrDown(img,  prefix, cnt, divisor)
+        image_reduce_resolution(img,  prefix, cnt, divisor, record_reduce)
 
 
 if __name__ == '__main__':
@@ -133,7 +40,8 @@ if __name__ == '__main__':
         print_size("bmp: ", bmp_name)
         img = cv2.imread(bmp_name, 0)
         for cnt in range(start_png_para, 10):
-            test_para_for_png(img, num, save_path, cnt)
-    drawHistogram()
+            test_para_for_png(img, num, save_path, cnt, record_raw)
+    drawHistogram_3(record_raw, record_div, record_reduce)
+    print(record_raw)
     print(record_div)
     print(record_reduce)
---
+++ scripts/OpenMV/Compression/helpers.py
@@ -0,0 +1,90 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_two_names(prefix, fuc, divisor):
+    result_name = "{0}_{1}_{2}.png".format(prefix, fuc, divisor)
+    restore_name = "{0}_{1}_{2}_restore.bmp".format(prefix, fuc, divisor)
+    return result_name, restore_name
+
+
+def print_size(str, file_path):
+    size = os.path.getsize(file_path)
+    # print(str, size)
+    return size
+
+
+def do_restrore(result_name, restore_name, multi_para=1):
+    cv2.imwrite(restore_name, cv2.imread(result_name, 0)*multi_para)
+    print_size("restore: ", restore_name)
+
+
+def print_value(rects, offset):
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x()+rect.get_width()/3. +
+                 offset, 1.01*height, '%s' % (height))
+
+
+# # 对图像作反色，几乎完全无效
+# # 对图像作除法，效果明显
+def image_div(img,  prefix, cnt, divisor, record, jump_restrore=False):
+    result_name, restore_name = get_two_names(prefix, "div", divisor)
+    cv2.imwrite(result_name, img/divisor, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+    record.append(print_size("div result: ", result_name))
+    if (jump_restrore == False):
+        do_restrore(result_name, restore_name, divisor)
+
+
+# 对图像降低分辨率，直接取左上角，效果明显
+def image_reduce_resolution(img,  prefix, cnt, divisor, record, jump_restrore=False):
+    result_name, restore_name = get_two_names(prefix, "reduce", divisor)
+
+    row = int(120/divisor)
+    col = int(160/divisor)
+    tmp = np.zeros((row, col))
+    for i in range(row):
+        for j in range(col):
+            tmp[i, j] = (img[i*divisor, j*divisor]).astype(np.uint8)
+
+    cv2.imwrite(result_name, tmp, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+    record.append(print_size("reduce result: ", result_name))
+    if (jump_restrore == False):
+        do_restrore(result_name, restore_name)
+
+
+# 对图像降低分辨率，cv2.pyrDown，太影响破损检测
+def image_pyrDown(img,  prefix, cnt, divisor, jump_restrore=False):
+    result_name, restore_name = get_two_names(prefix, "pyrDown", divisor)
+    tmp = img
+    for i in range(divisor-1):
+        tmp = cv2.pyrDown(tmp)
+    cv2.imwrite(result_name, tmp, [cv2.IMWRITE_PNG_COMPRESSION, cnt])
+    print_size("pyrDown result: ", result_name)
+    if (jump_restrore == False):
+        do_restrore(result_name, restore_name)
+
+
+def drawHistogram_3(list1, list2, list3, offset1=-0.05, offset2=-0.03, offset3=-0.05):
+    plt.rcParams["font.sans-serif"] = ['SimHei']  # 设置字体
+    plt.rcParams["axes.unicode_minus"] = False  # 正常显示负号
+    total_width, n = 0.5, 3   # 柱状图总宽度，有几组数据
+    width = total_width / n   # 单个柱状图的宽度
+    my_fontsize = 16
+    x = np.arange(len(list2))   # 横坐标范围
+
+    plt.figure()
+    plt.title("")
+    plt.xlabel("png压缩级别", fontsize=my_fontsize)
+    plt.xticks(x, range(10-len(list1), 10))
+    plt.ylabel("结果图片大小/Byte", fontsize=my_fontsize)
+    rect0 = plt.bar(x - width, list1, width=width, label="raw")
+    rect1 = plt.bar(x, list2, width=width, label="div 16")
+    rect2 = plt.bar(x + width, list3, width=width, label="reduce 3")
+    print_value(rect0, offset1)
+    print_value(rect1, offset2)
+    print_value(rect2, offset3)
+    plt.legend()
+    plt.show()
