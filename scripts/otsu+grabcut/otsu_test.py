81cb3ef1a2036da74cd13cd00cc0c5e42437420e
antarcticrobot <antarcticrobot@163.com>
2023-03-10 21:56:09 +0800

    增加多图的阈值获取mask图

--- scripts/otsu+grabcut/otsu_test.py
+++ scripts/otsu+grabcut/otsu_test.py
@@ -1,39 +1,52 @@
+import os
 import cv2 as cv
 import matplotlib.pyplot as plt
-# 1. 图像读取
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/1882626.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/1884624.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/1824640.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/2236399.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/2426441.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/1393595.jpg', 0)
-# img = cv.imread('/home/yr/热成像数据_存档_排烟管/2023_02_20_1630_pyg/raw/444743.jpg', 0)
-img = cv.imread('/home/yr/热成像数据_存档_排烟管/2023_02_20_1630_pyg/raw/421802.bmp', 0)
-
-# img = cv.imread('/home/yr/热成像数据_存档_排烟管/2023_02_20_1620_pyg/raw/444378.bmp',0)
 
 
-# 2.固定阈值 threshold(要处理的图像，一般是灰度图, 设定的阈值, 灰度中的最大值, 阈值分割的方式)
-ret, th1 = cv.threshold(img, 110, 255, cv.THRESH_BINARY)
-ret, th1_2 = cv.threshold(img, 120, 255, cv.THRESH_BINARY)
-ret, th1_3 = cv.threshold(img, 130, 255, cv.THRESH_BINARY)
-# 3. OSTU阈值 threshold(要处理的图像，一般是灰度图, 设定的阈值, 灰度中的最大值, 阈值分割的方式)
-# img = cv.imread('/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw/2426441.jpg', 0)
-th2 = cv.adaptiveThreshold(
-    img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3, 0)
+def get_img_num(directionName):
+    num_list = []
+    for parent, dirnames, filenames in os.walk(directionName):
+        for filename in filenames:
+            if filename.lower().endswith('.bmp'):
+                fname1, fname2 = os.path.split(filename)
+                num_list.append(str.split(fname2, '.bmp')[0])
+    return num_list
 
-ret2, th3 = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 
-cv.imwrite("./mask/421802.bmp", th3)
+def get_mask(bmp_name):
+    img = cv.imread(bmp_name, 0)
+    th1 = list((img, img, img))
+    for i in range(3):
+        ret, th1[i] = cv.threshold(img, 110+i*10, 255, cv.THRESH_BINARY)
 
-# 4. 结果绘制
-# plt.figure(figsize=(10,8),dpi=200, facecolor='white')
-plt.subplot(231), plt.imshow(img, 'gray'), plt.title('Original Imgae')
-plt.subplot(232), plt.imshow(th2, 'gray'), plt.title(
-    'Adaptive Threshold Segmentation')
-plt.subplot(233), plt.imshow(th3, 'gray'), plt.title('OStu segmentation')
+    th2 = cv.adaptiveThreshold(
+        img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3, 0)
 
-plt.subplot(234), plt.imshow(th1, 'gray'), plt.title('Fixed Threshold = 110')
-plt.subplot(235), plt.imshow(th1_2, 'gray'), plt.title('Fixed Threshold = 120')
-plt.subplot(236), plt.imshow(th1_3, 'gray'), plt.title('Fixed Threshold = 130')
-plt.show()
+    ret2, th3 = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+    cv.imwrite("./mask/"+str(num)+".bmp", th3)
+    plt.subplot(231), plt.imshow(img, 'gray'), plt.title('原始图像')
+    plt.subplot(232), plt.imshow(th2, 'gray'), plt.title('自适应阈值分割')
+    plt.subplot(233), plt.imshow(th3, 'gray'), plt.title('OStu阈值分割')
+    for i in range(3):
+        plt.subplot(
+            2, 3, 4+i), plt.imshow(th1[i], 'gray'), plt.title('固定阈值='+str(110+i*10))
+    plt.show()
+
+
+if __name__ == '__main__':
+    # num_list = [1882626, 1884624, 1824640, 2236399, 2426441, 1393595]
+    # read_path = '/home/yr/热成像数据_存档/2023_01_03_2110_tqyb4/raw'
+
+    # num_list = [421802, 444743]
+    # read_path = '/home/yr/热成像数据_存档_排烟管/外裹纸/2023_02_20_1630_pyg/raw/'
+
+    read_path = '/home/yr/catkin_ws/src/my_robot/selected_pic_for_test_pyg/'
+    num_list = get_img_num(read_path)
+
+    print(num_list)
+
+    for num in num_list:
+        bmp_name = read_path+str(num)+".bmp"
+        # bmp_name = read_path+str(num)
+        get_mask(bmp_name)
